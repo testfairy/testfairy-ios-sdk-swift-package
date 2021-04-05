0dc59a5f5024c76c10e523abf3afec4639dea899
Vijay Sharma <vijay.sharm@gmail.com>
2021-04-05 14:51:38 -0400

    Release 1.28.3 with iOS SDK 1.28.3 and Android SDK 1.12.3

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TestFairy",
-            url: "https://app.testfairy.com/ios-sdk/TestFairySDK-1.28.2.xcframework.zip",
-            checksum: "2cb2443c4cd585ad536c836044a375e72c2d3e4a9b9ed51079547564e040988f"
+            url: "https://app.testfairy.com/ios-sdk/TestFairySDK-1.28.3.xcframework.zip",
+            checksum: "b7a481da4ce076d0cd094bad953c32502ed97aed2225e931bf552d0866a61916"
         ),
     ]
 )
